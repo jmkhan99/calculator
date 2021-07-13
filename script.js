1f0ef14d54e50350dde137c0bd627ce2440242e9
jmkhan99 <jmkhana@gmail.com>
2021-07-13 18:44:29 +0900

    20210713 console log

--- script.js
+++ script.js
@@ -3,63 +3,161 @@
 const calculatorValue = document.querySelector(".calculator-value");
 const calculatorTable = document.querySelector(".calculator-table");
 const calculatorCell = document.querySelectorAll("td:not(.value-cell)");
-let nowValue = calculatorValue.innerText;
+const secondNumberViews = document.querySelector(".second-number-views");
+let operatorViews = document.querySelector(".operator-views");
+const firstOperandViews = document.querySelector(".first-operand-views");
+let firstOperand = null;
+let operatorVari = null;
+let waitingForSecondOperand = false;
+let secondNum = null;
+let nextOperatorVari = null;
 
-function addNumber(event) {
-  nowValue = nowValue + event.target.outerText;
-  calculatorValue.innerText = nowValue;
-  nowValue = nowValue;
-}
-function addOp(event) {
-  if (
-    calculatorValue.innerText[calculatorValue.innerText.length - 1] !== "+" &&
-    calculatorValue.innerText[calculatorValue.innerText.length - 1] !== "-" &&
-    calculatorValue.innerText[calculatorValue.innerText.length - 1] !== "*" &&
-    calculatorValue.innerText[calculatorValue.innerText.length - 1] !== "/"
-  ) {
-    nowValue = nowValue + event.target.outerText;
-    calculatorValue.innerText = nowValue;
-    nowValue = calculatorValue.innerText;
-    console.log("true");
+calculatorValue.innerText = "0";
+
+function addNumber(number) {
+  if (waitingForSecondOperand === true) {
+    console.log("addnumber second");
+    calculatorValue.innerText = number;
+    waitingForSecondOperand = false;
+    return;
   } else {
-    calculatorValue.innerText = nowValue.slice(0, -1);
+    console.log("addnumber first");
     calculatorValue.innerText =
-      calculatorValue.innerText + event.target.outerText;
-    console.log("false");
+      calculatorValue.innerText === "0"
+        ? number
+        : calculatorValue.innerText + number;
+  }
+}
+function addDot(dot) {
+  if (waitingForSecondOperand === true) {
+    calculatorValue.innerText = "0.";
+    waitingForSecondOperand = false;
+  }
+  if (!calculatorValue.innerText.includes(".")) {
+    calculatorValue.innerText = calculatorValue.innerText + dot;
+  }
+}
+function addOp(operator) {
+  if (firstOperand === null) {
+    console.log("first operand + operator");
+    firstOperand = calculatorValue.innerText;
+    firstOperandViews.innerText = firstOperand;
+    operatorVari = operator;
+    operatorViews.innerText = operatorVari;
+    waitingForSecondOperand = true;
+    return;
+  }
+  if (waitingForSecondOperand === true) {
+    console.log("change operator");
+    operatorVari = operator;
+    operatorViews.innerText = operatorVari;
+    return;
+  }
+  if (
+    operator === "+" ||
+    operator === "-" ||
+    operator === "*" ||
+    operator === "/"
+  ) {
+    console.log("go final");
+    secondNum = calculatorValue.innerText;
+    nextOperatorVari = operator;
+    console.log(nextOperatorVari);
+    finalNumber(firstOperand, secondNum, operatorVari);
+    return;
+  }
+  if (operator === "=") {
+    secondNum = calculatorValue.innerText;
+    nextOperatorVari = null;
+    console.log(nextOperatorVari);
+    finalNumber(firstOperand, secondNum, operatorVari);
+    return;
   }
 }
 function deleteNumber() {
-  calculatorValue.innerText = nowValue.slice(0, -1);
-  nowValue = calculatorValue.innerText;
+  calculatorValue.innerText = calculatorValue.innerText.slice(0, -1);
 }
 
 function deleteAllNumber() {
-  calculatorValue.innerText = "";
-  nowValue = "";
+  calculatorValue.innerText = "0";
+  firstOperand = null;
+  operatorVari = null;
+  waitingForSecondOperand = false;
+  secondNum = null;
+  firstOperandViews.innerText = "";
+  operatorViews.innerText = "";
 }
 
-function finalNumber() {
-  calculatorValue.innerText = new Function("return " + nowValue)();
-  nowValue = calculatorValue.innerText;
+function finalNumber(a, b, operator) {
+  if (operator === "+") {
+    console.log("final plus");
+    calculatorValue.innerText = parseFloat(
+      (parseFloat(a) + parseFloat(b)).toFixed(7)
+    );
+    firstOperand = calculatorValue.innerText;
+    firstOperandViews.innerText = firstOperand;
+    operatorVari = nextOperatorVari;
+    operatorViews.innerText = operatorVari;
+    waitingForSecondOperand = operatorVari !== null ? true : false;
+    return;
+  }
+  if (operator === "-") {
+    console.log("final -");
+    calculatorValue.innerText = parseFloat(
+      (parseFloat(a) - parseFloat(b)).toFixed(7)
+    );
+    firstOperand = calculatorValue.innerText;
+    firstOperandViews.innerText = firstOperand;
+    operatorVari = nextOperatorVari;
+    operatorViews.innerText = operatorVari;
+    waitingForSecondOperand = operatorVari !== null ? true : false;
+    return;
+  }
+  if (operator === "*") {
+    calculatorValue.innerText = parseFloat(
+      (parseFloat(a) * parseFloat(b)).toFixed(7)
+    );
+    firstOperand = calculatorValue.innerText;
+    firstOperandViews.innerText = firstOperand;
+    operatorVari = nextOperatorVari;
+    operatorViews.innerText = operatorVari;
+    waitingForSecondOperand = operatorVari !== null ? true : false;
+    return;
+  }
+  if (operator === "/") {
+    calculatorValue.innerText = parseFloat(
+      (parseFloat(a) / parseFloat(b)).toFixed(7)
+    );
+    firstOperand = calculatorValue.innerText;
+    firstOperandViews.innerText = firstOperand;
+    operatorVari = nextOperatorVari;
+    operatorViews.innerText = operatorVari;
+    waitingForSecondOperand = operatorVari !== null ? true : false;
+    return;
+  }
+  console.log("what final");
 }
 
-calculatorCell[2].addEventListener("click", deleteNumber);
-calculatorCell[1].addEventListener("click", deleteAllNumber);
-calculatorCell[19].addEventListener("click", finalNumber);
-
-calculatorCell[11].addEventListener("click", addOp);
-calculatorCell[15].addEventListener("click", addOp);
-calculatorCell[7].addEventListener("click", addOp);
-calculatorCell[3].addEventListener("click", addOp);
-
-calculatorCell[18].addEventListener("click", addNumber);
-calculatorCell[17].addEventListener("click", addNumber);
-calculatorCell[14].addEventListener("click", addNumber);
-calculatorCell[13].addEventListener("click", addNumber);
-calculatorCell[12].addEventListener("click", addNumber);
-calculatorCell[10].addEventListener("click", addNumber);
-calculatorCell[9].addEventListener("click", addNumber);
-calculatorCell[8].addEventListener("click", addNumber);
-calculatorCell[6].addEventListener("click", addNumber);
-calculatorCell[5].addEventListener("click", addNumber);
-calculatorCell[4].addEventListener("click", addNumber);
+calculatorTable.addEventListener("click", (event) => {
+  const target = event.target;
+  if (!target.matches("td")) {
+    return;
+  }
+  if (target.classList.contains("operator-keys")) {
+    addOp(target.outerText);
+    return;
+  }
+  if (target.classList.contains("dot")) {
+    addDot(target.outerText);
+    return;
+  }
+  if (target.classList.contains("delete-key")) {
+    deleteNumber(target.outerText);
+    return;
+  }
+  if (target.classList.contains("clear-key")) {
+    deleteAllNumber(target.outerText);
+    return;
+  }
+  addNumber(target.outerText);
+});
